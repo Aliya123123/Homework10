153730ab584f90a7470ad1dfa28e42099cfe0454
user <transakon77@gmail.com>
2024-08-13 19:40:15 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("task1");
+        String firstName = "для хранения имени";
+        firstName = " Ivan";
+        String middleName = "для хранения отчества";
+        middleName = " Ivanovich";
+        String lastName = "для хранения фамилии";
+        lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+
+        System.out.println("task2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("task3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameTrue);
     }
 }
