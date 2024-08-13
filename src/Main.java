030e5449e1cc2e68ae32982a159ec3043860380b
user <transakon77@gmail.com>
2024-08-13 20:54:04 +0300

    finish 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("task1");
-        String firstName = "для хранения имени";
-        firstName = " Ivan";
-        String middleName = "для хранения отчества";
-        middleName = " Ivanovich";
-        String lastName = "для хранения фамилии";
-        lastName = "Ivanov";
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
         String fullName = lastName + firstName + middleName;
         System.out.println(fullName);
 
